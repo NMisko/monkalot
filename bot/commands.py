720ab0b9fc52f4621d3f3d8bb17b8d4e9cfd4e5f
CodeMonkey3451 <alexander.ekrheg@gmail.com>
2017-08-05 19:10:43 +0200

    Added !call, !any, !word.

--- bot/commands.py
+++ bot/commands.py
@@ -134,9 +134,9 @@ class Spam(Command):
 
 
 class PyramidReply(Command):
-    '''Simple meta-command to output a reply with a pyramid given
-        a specific command. Basic key to value mapping.'''
-
+    """Simple meta-command to output a reply with a pyramid given
+    a specific command. Basic key to value mapping.
+    """
     perm = Permission.User
 
     replies = {
@@ -161,6 +161,73 @@ class PyramidReply(Command):
                 bot.write(reply + ' ' + reply)
                 bot.write(reply)
                 break
+
+
+class EmoteReply(Command):
+    """Output a msg with a specific emote. E.g.:
+    'Kappa NOW Kappa THATS Kappa A Kappa NICE Kappa COMMAND Kappa'
+    """
+    perm = Permission.User
+    cmd = ''
+    emote = ''
+    text = ''
+    """Maximum word/character values so chat doesnt explode."""
+    maxwords = [12, 15, 1]  # [call, any, word]
+    maxchars = [60, 80, 30]
+
+    def checkmaxvalues(self, cmd, text):
+        """Check if messages are in bounds."""
+
+        if cmd == '!call':
+            i = 0
+        elif cmd == '!any':
+            i = 1
+        elif cmd == '!word':
+            i = 2
+
+        return (len(self.text) <= self.maxchars[i] and len(self.text.split(' ')) <= self.maxwords[i])
+
+    def match(self, bot, user, msg):
+        """Msg has to have the structure !cmd <EMOTE> <TEXT>"""
+        if msg.lower().startswith('!call ') \
+        or msg.lower().startswith('!any ') \
+        or msg.lower().startswith('!word '):
+            parse = msg.split(' ', 2)
+            self.cmd = parse[0].strip()
+            self.emote = parse[1].strip()
+            if (self.emote in bot.emotes or self.emote in bot.emojis):
+                try:
+                    self.text = parse[2].strip()
+                except IndexError:
+                    return False    # No text
+                return self.checkmaxvalues(self.cmd, self.text)
+            else:
+                return False    # No emote
+        else:
+            return False
+
+    def run(self, bot, user, msg):
+        """Output emote message if cmd matches."""
+        if self.cmd == '!call':
+            parsetext = self.text.split(' ')
+            s = self.emote + ' NOW ' + self.emote + ' THIS ' + self.emote + ' IS ' + self.emote \
+            + ' WHAT ' + self.emote + ' I ' + self.emote + ' CALL ' + self.emote
+            for i in range(0, len(parsetext)):
+                s += ' ' + parsetext[i].upper() + ' ' + self.emote
+        elif self.cmd == '!any':
+            parsetext = self.text.split(' ')
+            s = self.emote
+            for i in range(0, len(parsetext)):
+                s += ' ' + parsetext[i].upper() + ' ' + self.emote
+        elif self.cmd == '!word':
+            parsetext = list(self.text)
+            s = self.emote
+            for i in range(0, len(parsetext)):
+                s += ' ' + parsetext[i].upper() + ' ' + self.emote
+        else:
+            return
+
+        bot.write(s)
 
 
 class EditCommandMods(Command):
