1543ad34e38db28c3191c97d8bb8b0bbf768d0d2
CodeMonkey3451 <alexander.ekrheg@gmail.com>
2017-09-08 20:03:39 +0200

    Reworked Speech CleverWrap objects only get reset if an error is caught. Each chatter gets his own CleverWrap object for better conversations.

--- bot/commands.py
+++ bot/commands.py
@@ -1261,22 +1261,25 @@ class Speech(Command):
     """Natural language by using cleverbot."""
 
     perm = Permission.User
-    cw = ""
+    cw = {}
     output = ""
 
     def getReply(self, bot, user, msg):
         """Get reply from cleverbot and post it in the channel."""
-        output = self.cw.say(msg)
+        output = self.cw[user].say(msg)
         if not random.randint(0, 3):
             output = output + " monkaS"
-        bot.write("@" + user + " " + output)
-        """Resetting CleverWrap Object to counter reoccuring bug.
-        Should think of a cleaner solution in the future."""
-        self.cw.reset()
+        try:
+            bot.write("@" + user + " " + output)
+        except ValueError:
+            print("CleverWrap Error, resetting object now.")
+            """Resetting CleverWrap Object to counter the reoccuring bug.
+            Should think of a cleaner solution in the future."""
+            self.cw[user].reset()
 
-    def __init__(self, bot):
-        """Initialize the command."""
-        self.cw = CleverWrap(bot.cleverbot_key)
+    #def __init__(self, bot):
+    #    """Initialize the command."""
+    #    self.cw = CleverWrap(bot.cleverbot_key)
 
     def match(self, bot, user, msg):
         """Match if the bot is tagged."""
@@ -1288,6 +1291,9 @@ class Speech(Command):
             msg = msg.lower()
             msg = msg.replace("@", '')
             msg = msg.replace(bot.nickname, '')
+
+            if user not in self.cw:
+                self.cw[user] = CleverWrap(bot.cleverbot_key, user)
 
             """Get reply in extra thread, so bot doesnt pause while waiting for the reply."""
             reactor.callInThread(self.getReply, bot, user, msg)
