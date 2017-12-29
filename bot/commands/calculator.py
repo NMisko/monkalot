45e2fd5f6f5cbd4d5667bfbd088a477a192d2e1f
Nicola Miskowiec <nicola.miskowiec@gmail.com>
2017-12-29 21:19:05 +0100

    Fixes #27
    0.1 + 0.2 = 0.3
    Calculation results get rounded to 5 decimal places AFTER the first non
    zero digit.

--- bot/commands/calculator.py
+++ bot/commands/calculator.py
@@ -1,12 +1,14 @@
 """Commands: "!calc"."""
 import re
 
+import math
 import pyparsing
 
 from bot.commands.command import Command
 from bot.utilities.math_parser import NumericStringParser
 from bot.utilities.permission import Permission
 
+PRECISION = 5
 
 class Calculator(Command):
     """A chat calculator that can do some pretty advanced stuff like sqrt and trigonometry.
@@ -33,8 +35,16 @@ class Calculator(Command):
         expr = msg.split(' ', 1)[1]
         try:
             result = self.nsp.eval(expr)
-            # if result.is_integer():
-            #     result = int(result)
+
+            # This condition fixes floating point errors, like 0.1 + 0.2 = 0.300...004
+            # Rounds to <PRECISION> digits after the first non zero digit
+            # E.g.: 0.30000004 -> 0.3
+            #       0.000030030004 -> 0.00003003
+            if abs(result) < 1:
+                dist = abs(int(math.log10(abs(result)))) # How many zeroes are after ., before a non zero digit
+                result = round(result, PRECISION + dist)
+            else:
+                result = round(result, PRECISION)
             reply = "{} = {}".format(expr, result)
             bot.write(reply)
         except ZeroDivisionError:
