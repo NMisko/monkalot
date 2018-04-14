7c426ed96d24ff8c8e9791b1c86624ccd7c8a491
Nicola Miskowiec <nicola.miskowiec@gmail.com>
2018-04-14 18:11:00 +0200

    Set all five pronouns, instead of only three

--- bot/commands/pronouns.py
+++ bot/commands/pronouns.py
@@ -8,7 +8,7 @@ from bot.utilities.permission import Permission
 class Pronouns(Command):
     """Allows changing gender pronouns for a user.
 
-    Usage: !g <USER> she her hers
+    Usage: !g <USER> she her her hers herself
     """
 
     perm = Permission.Admin
@@ -19,14 +19,14 @@ class Pronouns(Command):
 
     def match(self, bot, user, msg, tag_info):
         """Match if message starts with !g and has one argument."""
-        return msg.startswith("!g ") and len(msg.split(' ')) == 5
+        return msg.startswith("!g ") and len(msg.split(' ')) == 7
 
     def run(self, bot, user, msg, tag_info):
         """Add custom pronouns."""
         self.responses = bot.responses["Pronouns"]
         args = msg.lower().split(' ')
 
-        bot.pronouns[args[1]] = [args[2], args[3], args[4]]
+        bot.pronouns[args[1]] = [args[2], args[3], args[4], args[5], args[6]]
         with open(bot.pronouns_path.format(bot.root), 'w', encoding="utf-8") as file:
             json.dump(bot.pronouns, file, indent=4)
 
